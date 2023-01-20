bf5bb519d85c3488c166a6925bdfb240514e7e07
Fezeh-Moradi <80217691+Fezeh-Moradi@users.noreply.github.com>
2023-01-20 23:23:30 +0330

    Score and High Score is Done!
    Score and High Score is Done!

--- Plane/Assets/Scripts/Score Script/Score_Script.cs
+++ Plane/Assets/Scripts/Score Script/Score_Script.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+//using UnityEngine.SceneManagement; 
 
 public class Score_Script : MonoBehaviour
 {
 
     public int score = 0;
     public Text scoreText;
+    public int highScore;
+    //public static int lastScore;
+
+
+    
+    public Text highScoreText;
+    //public Text LastScoreText;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Score());
+        //StartCoroutine(Reload());
+        //lastScore = 0;
+        highScore = PlayerPrefs.GetInt("high_score", 0);
+        highScoreText.text ="High Score : " + highScore.ToString();
+        //LastScoreText.text ="Last Score : " + lastScore.ToString();
         
     }
 
@@ -19,14 +32,27 @@ public class Score_Script : MonoBehaviour
     void Update()
     {
         scoreText.text = score.ToString(); 
+        if(score > highScore){
+            highScore = score;
+            PlayerPrefs.SetInt("high_score" , highScore);
+        }
     }
 
     IEnumerator Score(){
         while(true){
             yield return new WaitForSeconds(2);
             score = score + 1;
-            Debug.Log("Score : " + score);
+           // lastScore = score;
             
         }
     }
+
+
+    /*IEnumerator Reload(){
+        
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("");
+            
+        
+    }*/
 }
